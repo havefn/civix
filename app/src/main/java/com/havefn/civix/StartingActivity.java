166aecc6f2095f244f60fcb688aa6648d31d7681
affandhia <affandhia@gmail.com>
2016-06-18 15:29:56 +0700

    Added color scheme with its shades.

--- app/src/main/java/com/havefn/civix/StartingActivity.java
+++ app/src/main/java/com/havefn/civix/StartingActivity.java
@@ -11,21 +11,11 @@ import android.widget.Button;
  */
 public class StartingActivity extends AppCompatActivity {
 
-    protected Button btnSignin;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
 
-        btnSignin = (Button) findViewById(R.id.btn_to_register);
 
-        btnSignin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(StartingActivity.this, RegistrationActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
